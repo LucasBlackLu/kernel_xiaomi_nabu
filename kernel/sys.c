bd6b9d1416cbc5d0eebfc88853299231d098dce0
Tim Zimmermann <tim@linux4.de>
2025-06-13 16:35:59 +0800

    [Android] bpf: Spoofing the netd-bpfloader for linux version 5.4.200
    Change-Id: I0e9f0c929d23e4d1e0c49a7d72a35799cddecc43

--- kernel/sys.c
+++ kernel/sys.c
@@ -1191,9 +1191,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
+
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4))
+		strcpy(tmp.release, "5.4.200");
+
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
